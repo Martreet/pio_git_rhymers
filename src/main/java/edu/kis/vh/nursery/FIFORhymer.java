c012922440847af1e9102b6dbbf38076fa7321d0
Martreet <mik.turek@gmail.com>
2022-01-03 14:59:31 +0100

    1.3.5 Zmiana nazw klas na dużą literę

--- src/main/java/edu/kis/vh/nursery/FIFORhymer.java
+++ src/main/java/edu/kis/vh/nursery/FIFORhymer.java
@@ -1,8 +1,8 @@
 package edu.kis.vh.nursery;
 
-public class FIFORhymer extends defaultCountingOutRhymer {
+public class FIFORhymer extends DefaultCountingOutRhymer {
 
-	public defaultCountingOutRhymer temp = new defaultCountingOutRhymer();
+	public DefaultCountingOutRhymer temp = new DefaultCountingOutRhymer();
 
 	@Override
 	public int countOut() {
@@ -18,4 +18,4 @@ public class FIFORhymer extends defaultCountingOutRhymer {
 
 		return ret;
 	}
-}
+
